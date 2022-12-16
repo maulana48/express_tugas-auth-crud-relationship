6bb3de703acb5dcb26ca7e6a98da272643e01906
maulana48 <hidarizero4869@gmail.com>
2022-12-16 09:33:06 +0700

    first-commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+const db = require("./app/models");
+const Role = db.role;
+
+
+var corsOptions = {
+    origin: "http://localhost:3001"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to bezkoder application."
+    });
+});
+
+
+
+
+// untuk prod : db.sequelize.sync();
+db.sequelize.sync({
+    force: true
+}).then(() => {
+    console.log('Drop and Resync Db');
+    initial();
+});
+
+function initial() {
+    Role.create({
+        id: 1,
+        name: "user"
+    });
+
+    Role.create({
+        id: 2,
+        name: "moderator"
+    });
+
+    Role.create({
+        id: 3,
+        name: "admin"
+    });
+}
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
